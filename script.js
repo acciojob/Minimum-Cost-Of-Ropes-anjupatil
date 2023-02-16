742da294fc091752755a47ede293ef69720dc47b
Anjali Patil <anjupatil42@gmail.com>
2023-02-16 16:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 //your code here
-     function RopeCost(event){
-	 event.preventDefault();
-	 var input=document.querySelector('input').value;
-	 var arr=input.split(",");
-	 var cost=0;
-	 arr.sort(function (a,b) {return a-b});
-
-	 while(arr.length>1){
-		 var res=Number(arr[0])+Number(arr[1]);
-		 arr.push(res);
-		 cost+=res;
-		 arr.shift();
-		 arr.shift();
-		 arr.sort(function (a,b) {return a-b});
-	 }
-	 document.getElementById('result').textContent=cost;
- }
+   function minCostToFormRopes(event){
+	
+	event.preventDefault();
+	var inputElement = document.querySelector('input').value;
+    var arr = inputElement.split(',');
+	arr.sort(function (a,b) {return a-b});
+	
+	var cost = 0;
+	while(arr.length > 1){
+		var res =Number(arr[0]) + Number(arr[1]);
+		arr.push(res);
+		cost += res;
+		arr.shift();
+		arr.shift();
+	    arr.sort(function (a,b) {return a-b});
+	}
+	document.getElementById("result").textContent = cost;
+}
